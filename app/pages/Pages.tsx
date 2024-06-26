e04bfa63c94459ef2a5957a8baff717627ee3bea
reyhermanses <rey.undot@gmail.com>
2024-06-27 06:53:54 +0700

    delete folder pages

--- app/pages/Pages.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-
-const Pages = () => {
-  return <div>Pages</div>;
-};
-
-export default Pages;
--- app/pages/profile/Profile.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./page.module.css";
-
-const Profile = () => {
-  return <div className={styles.description}>HOME</div>;
-};
-
-export default Profile;
